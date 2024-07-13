1c92aba1bb626315c06edd54b7f13b5e4bec0dfb
MohamedElmelegy74 <75188929+MohamedElmelegy74@users.noreply.github.com>
2024-07-13 22:56:37 +0300

    FeachNewstBooksUseCase

---
+++ lib/Features/Domin/Use Cases/Home/feach_newst_books.dart
@@ -0,0 +1,17 @@
+import 'package:book_with_claen_architecture/core/UseCase/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failuer.dart';
+import '../../Entities/book_entities.dart';
+import '../../Repositories/Home/home_repo.dart';
+
+class FeachNewstBooksUseCase extends UseCase<List<BookEntities>, NoParm> {
+  final HomeRepo homeRepo;
+//UseCase To Repo(S ->Single Responsibility Principle. )-->A class should have only one reason to change.
+  FeachNewstBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failuer, List<BookEntities>>> call([NoParm? param]) async {
+    return await homeRepo.feachNewestBooks();
+  }
+}
